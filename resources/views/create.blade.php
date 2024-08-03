9456af00382bd4b591d3c674b7c70bfda6674093
SamMathaniha <mathanihasam@gmail.com>
2024-08-03 23:23:11 +0530

    added php route

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,22 +2,22 @@
 @section('content')
 <div class="container mt-5">
     <h2>Create New Employee</h2>
-    <form>
+    <form action="" method="POST" novalidate>
         <div class="form-group">
             <label for="name">Name</label>
-            <input type="text" class="form-control" id="name" placeholder="Enter name" required>
+            <input type="text" name="Name" class="form-control" id="name" placeholder="Enter name" required>
         </div>
         <div class="form-group">
             <label for="email">Email</label>
-            <input type="email" class="form-control" id="email" placeholder="Enter email" required>
+            <input type="email" name="Email" class="form-control" id="email" placeholder="Enter email" required>
         </div>
         <div class="form-group">
             <label for="joiningDate">Joining Date</label>
-            <input type="date" class="form-control" id="joiningDate" required>
+            <input type="date" name="joiningDate"name="Email" class="form-control" id="joiningDate" required>
         </div>
         <div class="form-group">
             <label for="salary">Salary</label>
-            <input type="number" class="form-control" id="salary" placeholder="Enter salary" required>
+            <input type="number" name="Salary" class="form-control" id="salary" placeholder="Enter salary" required>
         </div>
         <div class="form-check mb-3">
             <input type="checkbox" class="form-check-input" id="active">
