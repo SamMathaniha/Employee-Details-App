9fbcb0f9348f20befe204b41aed3cf5971f0d9a3
SamMathaniha <mathanihasam@gmail.com>
2024-08-03 22:02:45 +0530

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ Route::get('/', function () {
 
 Route::get('/employee',[EmployeeController::class,'index']);
 Route::get('/employee/create',[EmployeeController::class,'create']);
+Route::get('/employee/edit',[EmployeeController::class,'edit']);
+Route::get('/employee/show',[EmployeeController::class,'show']);
 
