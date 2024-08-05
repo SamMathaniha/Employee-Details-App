da1ae2dad0b8d88eeb86781178468ba9936475e7
SamMathaniha <mathanihasam@gmail.com>
2024-08-05 17:06:35 +0530

    Added invalid errors for the feilds in update

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -5,7 +5,7 @@
     <form action="update.php" method="POST">
         <div class="form-group">
             <label for="name">Name:</label>
-            <input type="text" class="form-control" id="name" name="name" value="{{$employee->name}}">
+            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{$employee->name}}">
             @error('name')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -14,7 +14,7 @@
         </div>
         <div class="form-group">
             <label for="email">Email:</label>
-            <input type="email" class="form-control" id="email" name="email" value="{{$employee->email}}">
+            <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{$employee->email}}">
             @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -23,7 +23,7 @@
         </div>
         <div class="form-group">
             <label for="joining_date">Joining Date:</label>
-            <input type="date" class="form-control" id="joining_date" name="joining_date" value="{{$employee->joining_date}}">
+            <input type="date" class="form-control @error('joining_date') is-invalid @enderror" id="joining_date" name="joining_date" value="{{$employee->joining_date}}">
             @error('joining_date')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -32,7 +32,7 @@
         </div>
         <div class="form-group">
             <label for="salary">Salary:</label>
-            <input type="text" class="form-control" id="salary" name="salary" value="{{$employee->salary}}">
+            <input type="text" class="form-control @error('salary') is-invalid @enderror" id="salary" name="salary" value="{{$employee->salary}}">
             @error('salary')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -41,7 +41,7 @@
         </div>
         <div class="form-group">
             <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="active" name="is_active" value="1" {{$employee->is_active == '1' ? 'checked' : ''}}>
+                <input type="checkbox" class="form-check-input" id="active" class="@error('is_active') is-invalid @enderror" name="is_active" value="1" {{$employee->is_active == '1' ? 'checked' : ''}}>
                 <label class="form-check-label" for="active">Active</label>
                 @error('is_active')
                 <span class="invalid-feedback" role="alert">
