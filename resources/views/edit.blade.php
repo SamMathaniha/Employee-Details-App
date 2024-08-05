fe4d64ee7e3aa34fffde79aac84bd3311ab4d30d
SamMathaniha <mathanihasam@gmail.com>
2024-08-05 17:01:19 +0530

    Added Error mgs same as create

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -6,23 +6,48 @@
         <div class="form-group">
             <label for="name">Name:</label>
             <input type="text" class="form-control" id="name" name="name" value="{{$employee->name}}">
+            @error('name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
         <div class="form-group">
             <label for="email">Email:</label>
             <input type="email" class="form-control" id="email" name="email" value="{{$employee->email}}">
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
         <div class="form-group">
             <label for="joining_date">Joining Date:</label>
             <input type="date" class="form-control" id="joining_date" name="joining_date" value="{{$employee->joining_date}}">
+            @error('joining_date')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
         <div class="form-group">
             <label for="salary">Salary:</label>
             <input type="text" class="form-control" id="salary" name="salary" value="{{$employee->salary}}">
+            @error('salary')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
         <div class="form-group">
             <div class="form-check">
                 <input type="checkbox" class="form-check-input" id="active" name="is_active" value="1" {{$employee->is_active == '1' ? 'checked' : ''}}>
                 <label class="form-check-label" for="active">Active</label>
+                @error('is_active')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
             </div>
         </div>
         <button type="submit" class="btn btn-primary">Update</button>
